32056f101c3773bc6e6414ec9f71a82fced45e7e
Andy <649925097@qq.com>
2017-01-10 09:11:39 +0800

    2017年1月10日09:11:36

---
+++ Struts-start/src/struts2/login/LoginAction2.java
@@ -0,0 +1,18 @@
+package struts2.login;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginAction2 {
+	private String username;
+	private String password;
+	public String execute() {
+		if (this.getUsername().equals("admin2")&&this.getPassword().equals("8888")) {
+			return "success";
+		}
+		return "error";
+	}
+
+}
